e082501b4c1b3e3e511eb0ac039766315e3076c6
Bogar Martinez <bogar.cm@icloud.com>
2023-06-10 21:07:06 -0500

    scoping stuff

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution in this file!
+var customerName = "bob"
+function upperCaseCustomerName(){ 
+   return customerName = "BOB";
+}
+
+function setBestCustomer(x){
+   x= window.bestCustomer ='not bob';
+}
+function overwriteBestCustomer(x){
+    x = window.bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'dude';
+
+function changeLeastFavoriteCustomer(x){
+x = leastFavoriteCustomer = 'some other dude'
+}
